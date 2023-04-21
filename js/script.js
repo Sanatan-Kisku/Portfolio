b9a4d5bfae1730ff8ab8e1739c81c16e9c26427b
Sanatan Kisku <sanatankisku@gmail.com>
2023-04-21 23:26:50 +0530

    Add javascript and responsive stylesheet

--- js/script.js
+++ js/script.js
@@ -1,3 +1,12 @@
+// Toggle Icon Navbar
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+  menuIcon.classList.toggle('bx-x');
+  navbar.classList.toggle('active');
+};
+
 // Scroll Section Active Link
 let sections = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('header nav a');
@@ -16,4 +25,12 @@ window.onscroll = () => {
       });
     };
   });
+  // Sticky Navbar
+  let header = document.querySelector('header');
+
+  header.classList.toggle('sticky', window.scrollY > 100);
+
+  // remove toggle icon and navbar when section click (scroll)
+  menuIcon.classList.remove('bx-x');
+  navbar.classList.remove('active');
 };
